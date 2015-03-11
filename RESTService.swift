9fbd347754f620f0f39a80ff0cb4d333049be675
panleya <jason.blood@me.com>
2015-03-11 14:41:57 -0700

    Removed Copyright
    This version is free for the world. You’re welcome.

--- RESTService.swift
+++ RESTService.swift
@@ -1,9 +1,7 @@
 //
 //  RESTService.swift
-//  Photon
 //
 //  Created by Jason Blood on 3/11/15.
-//  Copyright (c) 2015 Photon Medical Communications Inc. All rights reserved.
 //
 
 import Foundation
